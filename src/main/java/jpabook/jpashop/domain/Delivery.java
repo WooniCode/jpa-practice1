76adc3f5b5f28d85ae2fa80b03d8d1b4d2491c54
woonijung <jdu2038@gmail.com>
2021-08-20 19:11:22 +0900

    ~ 상품 도메인 개발

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // String으로 써야 중간에 타입이 늘어도 상관 없음
+    private DeliveryStatus status; // READY, COMP
+}
