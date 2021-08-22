d3be4d96faea34f0fcd2ed88535981ff86835bb4
woonijung <jdu2038@gmail.com>
2021-08-23 08:35:41 +0900

    ~ 변경 감지와 Merge

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,19 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); // 이렇게 가져오면 영속 상태에 놓이고
+
+        // setter 대신 findItem.change(name, price, stockQuantity)로 메서드를 만들어 관리해야 소스 추적이 쉽다.
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        // 영속성 컨텍스트로 관리되고 있기 때문에 변경 감지(더티 체킹)가 되어 업데이트가 된다.
+
+        return findItem;
     }
 
     public List<Item> findItems() {
